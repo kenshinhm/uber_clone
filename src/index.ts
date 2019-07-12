9aab97df9940962e6be3794645f200de5055fb58
jaehyoungyoo <kenshinhm@naver.com>
2019-07-12 23:11:55 +0900

    move folder one step down

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
+
 dotenv.config();
+import {Options} from "graphql-yoga";
+import {createConnection} from "typeorm";
 import app from "./app";
-import { Options } from "graphql-yoga";
-import { createConnection } from "typeorm";
 import connectionOptions from "./ormConfig";
 
 const PORT: number | string = process.env.PORT || 4000;
@@ -10,16 +11,16 @@ const PLAYGROUND: string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
 
 const appOptions: Options = {
-  port: PORT,
-  playground: PLAYGROUND,
-  endpoint: GRAPHQL_ENDPOINT
+    port: PORT,
+    playground: PLAYGROUND,
+    endpoint: GRAPHQL_ENDPOINT
 };
 
 const handleAppStart = () =>
-  console.log(`Server runnig on http://localhost:${PORT}/playground`);
+    console.log(`Server running on http://localhost:${PORT}/playground`);
 
 createConnection(connectionOptions)
-  .then(() => {
-    app.start(appOptions, handleAppStart);
-  })
-  .catch(err => console.log(err));
+    .then(() => {
+        app.start(appOptions, handleAppStart);
+    })
+    .catch(err => console.log(err));
