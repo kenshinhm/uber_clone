d8c18c83c2509d6613743a2d09291b496fef4aac
jaehyoungyoo <kenshinhm@naver.com>
2019-07-13 00:49:23 +0900

    hashing & encrypting & verifying user password

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-import {Options} from "graphql-yoga";
-import {createConnection} from "typeorm";
+import { Options } from "graphql-yoga";
+import { createConnection } from "typeorm";
 import app from "./app";
 import connectionOptions from "./ormConfig";
 
@@ -11,16 +11,16 @@ const PLAYGROUND: string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
 
 const appOptions: Options = {
-    port: PORT,
-    playground: PLAYGROUND,
-    endpoint: GRAPHQL_ENDPOINT
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: GRAPHQL_ENDPOINT
 };
 
 const handleAppStart = () =>
-    console.log(`Server running on http://localhost:${PORT}/playground`);
+  console.log(`Server running on http://localhost:${PORT}/playground`);
 
 createConnection(connectionOptions)
-    .then(() => {
-        app.start(appOptions, handleAppStart);
-    })
-    .catch(err => console.log(err));
+  .then(() => {
+    app.start(appOptions, handleAppStart);
+  })
+  .catch(err => console.log(err));
