99b5b04558e29a476e5eaef21a4e8913871c91d4
jaehyoungyoo <kenshinhm@naver.com>
2019-07-20 00:01:46 +0900

    Custom Auth Middleware on Express

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,34 @@ import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
+import decodeJWT from "./utils/decodeJWT";
 
 class App {
-  public app: GraphQLServer;
+    public app: GraphQLServer;
 
-  constructor() {
-    this.app = new GraphQLServer({
-      schema: schema
-    });
-    this.middlewares();
-  }
+    constructor() {
+        this.app = new GraphQLServer({
+            schema: schema
+        });
+        this.middlewares();
+    }
 
-  private middlewares = (): void => {
-    this.app.express.use(cors());
-    this.app.express.use(logger("dev"));
-    this.app.express.use(helmet());
-  };
+    private middlewares = (): void => {
+        this.app.express.use(cors());
+        this.app.express.use(logger("dev"));
+        this.app.express.use(helmet());
+        this.app.express.use(this.jwt);
+    };
+
+    private jwt = async (req, res, next): Promise<void> => {
+        const token = req.get("X-JWT");
+        console.log("here");
+        if (token) {
+            const user = await decodeJWT(token);
+            console.log(user);
+        }
+        next();
+    };
 }
 
 export default new App().app;
