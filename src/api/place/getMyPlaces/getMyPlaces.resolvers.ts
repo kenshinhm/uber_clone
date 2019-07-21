52739a473110c01a104d815a43297457ef17639d
jaehyoungyoo <kenshinhm@naver.com>
2019-07-21 16:00:20 +0900

    GetMyPlaces Resolvers

---
+++ src/api/place/getMyPlaces/getMyPlaces.resolvers.ts
@@ -0,0 +1,40 @@
+import User from "../../../entities/user";
+import {GetMyPlacesResponse} from "../../../types/graph";
+import {Resolvers} from "../../../types/resolver";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+    Query: {
+        GetMyPlaces: privateResolver(
+            async (_, __, {req}): Promise<GetMyPlacesResponse> => {
+                try {
+                    const user = await User.findOne(
+                        {id: req.user.id},
+                        {relations: ["places"]}
+                    );
+                    if (user) {
+                        return {
+                            ok: true,
+                            places: user.places,
+                            error: null,
+                        }
+                    } else {
+                        return {
+                            ok: false,
+                            places: null,
+                            error: "user not found",
+                        }
+                    }
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message,
+                        places: null
+                    }
+                }
+            }
+        )
+    }
+};
+
+export default resolvers;
