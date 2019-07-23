f4d3532e0bcb6370b01ecda59997c2d237b935ac
jaehyoungyoo <kenshinhm@naver.com>
2019-07-23 23:32:13 +0900

    Authenticating WebSocket Subscription

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,44 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-import { Options } from "graphql-yoga";
-import { createConnection } from "typeorm";
+import {Options} from "graphql-yoga";
+import {createConnection} from "typeorm";
 import app from "./app";
 import connectionOptions from "./ormConfig";
+import decodeJWT from "./utils/decodeJWT";
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND: string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
+const SUBSCRIPTION_ENDPOINT: string = "/subscription";
 
 const appOptions: Options = {
-  port: PORT,
-  playground: PLAYGROUND,
-  endpoint: GRAPHQL_ENDPOINT
+    port: PORT,
+    playground: PLAYGROUND,
+    endpoint: GRAPHQL_ENDPOINT,
+    subscriptions: {
+        path: SUBSCRIPTION_ENDPOINT,
+        onConnect: async connectionParams => {
+            const token = connectionParams["X-JWT"];
+            if (token) {
+                const user = await decodeJWT(token);
+                if (user) {
+                    return {
+                        currentUser: user
+                    };
+                }
+            }
+            throw new Error("No JWT, Can't subscribe");
+        }
+    }
+
 };
 
 const handleAppStart = () =>
-  console.log(`Server running on http://localhost:${PORT}/playground`);
+    console.log(`Server running on http://localhost:${PORT}/playground`);
 
 createConnection(connectionOptions)
-  .then(() => {
-    app.start(appOptions, handleAppStart);
-  })
-  .catch(err => console.log(err));
+    .then(() => {
+        app.start(appOptions, handleAppStart);
+    })
+    .catch(err => console.log(err));
