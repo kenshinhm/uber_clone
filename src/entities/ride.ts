11551b07fd3a199a8befbf4e22f3928e95522cb2
jaehyoungyoo <kenshinhm@naver.com>
2019-07-27 12:29:26 +0900

    RequestRide

--- src/entities/ride.ts
+++ src/entities/ride.ts
@@ -1,64 +1,64 @@
-import { rideStatus } from "../types/types";
-import User from "./user";
 import {
-  BaseEntity,
-  Entity,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
-  Column,
-  ManyToOne
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
 } from "typeorm";
+import {rideStatus} from "../types/types";
+import User from "./user";
 
 @Entity()
 class Ride extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({
-    type: "text",
-    enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"]
-  })
-  status: rideStatus;
+    @Column({
+        type: "text",
+        enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"]
+    })
+    status: rideStatus;
 
-  @Column({ type: "text" })
-  pickUpAddress: string;
+    @Column({type: "text"})
+    pickUpAddress: string;
 
-  @Column({ type: "double precision", default: 0 })
-  pickUpLat: number;
+    @Column({type: "double precision", default: 0})
+    pickUpLat: number;
 
-  @Column({ type: "double precision", default: 0 })
-  pickUpLng: number;
+    @Column({type: "double precision", default: 0})
+    pickUpLng: number;
 
-  @Column({ type: "text" })
-  dropOffAddress: string;
+    @Column({type: "text"})
+    dropOffAddress: string;
 
-  @Column({ type: "double precision", default: 0 })
-  dropOffLat: number;
+    @Column({type: "double precision", default: 0})
+    dropOffLat: number;
 
-  @Column({ type: "double precision", default: 0 })
-  dropOffLng: number;
+    @Column({type: "double precision", default: 0})
+    dropOffLng: number;
 
-  @Column({ type: "double precision", default: 0 })
-  price: number;
+    @Column({type: "double precision", default: 0})
+    price: number;
 
-  @Column({ type: "text" })
-  distance: string;
+    @Column({type: "text"})
+    distance: string;
 
-  @Column({ type: "text" })
-  duration: string;
+    @Column({type: "text"})
+    duration: string;
 
-  @ManyToOne(type => User, user => user.ridesAsPassenger)
-  passenger: User;
+    @ManyToOne(type => User, user => user.ridesAsPassenger)
+    passenger: User;
 
-  @ManyToOne(type => User, user => user.ridesAsDriver)
-  driver: User;
+    @ManyToOne(type => User, user => user.ridesAsDriver, {nullable: true})
+    driver: User;
 
-  @CreateDateColumn()
-  createdAt: string;
+    @CreateDateColumn()
+    createdAt: string;
 
-  @UpdateDateColumn()
-  updatedAt: string;
+    @UpdateDateColumn()
+    updatedAt: string;
 }
 
 export default Ride;
